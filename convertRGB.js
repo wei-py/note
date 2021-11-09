cc44f10ab7d98f0228c13621bf78851feeb08a49
weipy <weipy6208@outlook.com>
2021-11-09 21:44:38 +0800

    update RGB

--- convertRGB.js
+++ convertRGB.js
@@ -1,6 +1,5 @@
 // 4、CSS颜色转换
 // #0000FF => rgb(0, 0, 255)
-
 function convertRGB(color) {
     color = color.replace('#', '');
     let len = color.length / 3;
@@ -8,23 +7,15 @@ function convertRGB(color) {
     color = color.split(r)
     let result = 'rgb('
     for (let c of color) {
-        result += Number('0x' + c).toString() + ', '
+        result += Number('0x' + c) + ', '
     }
     return result.replace(/(.*),/, '$1)');
 }
+
+
 
 // var color = '#0000FF';
 var color = '#FAAF39';
 color = '#0000FF'
 var a = convertRGB(color);
 console.log(a);
-
-
-// if (color.length == 7) {
-//     color = color.slice(1).split(/(?=(?:\w{2})+\b)/g);
-// }
-// else if (color.length == 4) {
-//     color = color.slice(1).split(/(?=(?:\w{1})+\b)/g);
-// } else {
-//     return false
-// }
--- draft1.js
+++ draft1.js
@@ -1,24 +1,6 @@
-var findMaxForm = function (strs, m, n) {
-    const dp = Array.from(Array(m + 1), () => Array(n + 1).fill(0));
-    let nums0 = 0;
-    let nums1 = 0;
-    for (let str of strs) {
-        nums0 = str.split('0').length - 1;
-        nums1 = str.split('1').length - 1;
-        // for (let i = m, j = n; i >= nums0 && j >= nums1; i--, j--) {
-        //     dp[i][j] = Math.max(dp[i][j], dp[i - nums0][j - nums1] + 1);
-        // }
-        for (let i = m; i >= nums0; i--) {
-            for (let j = n; j >= nums1; j--) {
-                dp[i][j] = Math.max(dp[i][j], dp[i - nums0][j - nums1] + 1);
-            }
-        }
+class mypromise {
+    constructor(handle) {
+        this.status = 'pending'
+
     }
-    console.log(dp);
-};
-
-
-// var strs = ["10", "0001", "111001", "1", "0"], m = 5, n = 3
-var strs = ["10", "0", "1"], m = 1, n = 1
-findMaxForm(strs, m, n)
-
+}
