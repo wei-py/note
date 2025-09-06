23029ffa5bc480f708392090a5101472a55300bd
wei-py <rabbwei@outlook.com>
2025-09-06 08:33:41 +0800

    interview

--- react-course-code-main/react-spring-test/src/App6.tsx
+++ react-course-code-main/react-spring-test/src/App6.tsx
@@ -1,10 +1,16 @@
-import { animated, useChain, useSpring, useSpringRef, useSprings, useTrail } from '@react-spring/web'
-import './App.css';
+import {
+  animated,
+  useChain,
+  useSpring,
+  useSpringRef,
+  useSprings,
+  useTrail,
+} from "@react-spring/web";
+import "./App.css";
 
 export default function App() {
+  const api1 = useSpringRef();
 
-  const api1 = useSpringRef()
-  
   const [springs] = useTrail(
     3,
     () => ({
@@ -12,32 +18,37 @@ export default function App() {
       from: { width: 0 },
       to: { width: 300 },
       config: {
-        duration: 1000
-      }
+        duration: 1000,
+      },
     }),
     []
-  )
+  );
 
-  const api2 = useSpringRef()
-  
+  const api2 = useSpringRef();
+
   const [springs2] = useSprings(
     3,
     () => ({
       ref: api2,
       from: { height: 100 },
-      to: { height: 50},
+      to: { height: 50 },
       config: {
-        duration: 1000
-      }
+        duration: 1000,
+      },
     }),
     []
-  )
+  );
 
-  useChain([api1, api2], [0, 1], 500)
+  useChain([api1, api2], [0, 1], 500);
 
-  return <div>
-    {springs.map((styles1, index) => (
-      <animated.div style={{...styles1, ...springs2[index]}} className='box'></animated.div>
-    ))}
-  </div>
+  return (
+    <div>
+      {springs.map((styles1, index) => (
+        <animated.div
+          style={{ ...styles1, ...springs2[index] }}
+          className="box"
+        ></animated.div>
+      ))}
+    </div>
+  );
 }
